779078790cd8874d39cab549671d9d91974409aa
Patrick Lohan <patrick.lohan@zoho.com>
2019-10-21 18:33:26 +0100

    Adds hardcoded states

--- rocknrollem_front/src/containers/MainContainer.js
+++ rocknrollem_front/src/containers/MainContainer.js
@@ -1,7 +1,26 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import CharacterContainer from './CharacterContainer';
 
-const MainContainer = () => {
+class MainContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      identity: [
+        'Mhairi',
+        'Clyde',
+        'Chaotic Neutral'
+      ],
+      race: 'Gnome',
+      class: 'Barbarian',
+      attributes: [17,12,15,8,10,10],
+      background: [
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      ]
+    }
+  }
 
   return (
     <Fragment>
