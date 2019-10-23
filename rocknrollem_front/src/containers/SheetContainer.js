f6ee9fbc99e222638d8574e92a9a1cabb3d48fa1
Patrick Lohan <patrick.lohan@zoho.com>
2019-10-23 11:24:33 +0100

    Adds equipment component

--- rocknrollem_front/src/containers/SheetContainer.js
+++ rocknrollem_front/src/containers/SheetContainer.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import Identity from "../components/character_sheet/Identity";
 import Stats from "../components/character_sheet/Stats";
 import Attributes from "../components/character_sheet/Attributes";
+import Equipment from '../components/character_sheet/Equipment';
 import Background from "../components/character_sheet/Background";
 
 class SheetContainer extends Component {
@@ -10,7 +11,8 @@ class SheetContainer extends Component {
       <Fragment>
         <Identity {...this.props.character} />
         <Stats />
-      <Attributes {...this.props.characterStats} />
+        <Attributes {...this.props.characterStats} />
+      <Equipment {...this.props.character.equipment} />
         <Background {...this.props.character.background} />
       </Fragment>
     );
